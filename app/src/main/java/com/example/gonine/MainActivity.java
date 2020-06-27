8ecd6f358c6231ed926c7afc837805f9fd24c9f4
qky18 <qky18@mails.tsinghua.edu.cn>
2020-06-27 22:02:29 +0800

    front-end xml, activity

---
+++ app/src/main/java/com/example/gonine/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.gonine;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.gonine.activity.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    //返回按钮
+    private TextView tv_back;
+    //登录按钮
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        init();
+    }
+
+    private void init() {
+        //从main_title_bar.xml 页面布局中获取对应的UI控件（已删除）
+        //布局根元素（已删除）
+        //从activity_main.xml 页面中获取对应的UI控件
+        tv_back=findViewById(R.id.tv_back);
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+}
