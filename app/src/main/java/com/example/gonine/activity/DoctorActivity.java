21b6d74a7e9bc3d36b43c0bec41d4dc512a110ab
qky18 <qky18@mails.tsinghua.edu.cn>
2020-06-30 21:21:53 +0800

    add doctor view & reusable toolbar

---
+++ app/src/main/java/com/example/gonine/activity/DoctorActivity.java
@@ -0,0 +1,165 @@
+package com.example.gonine.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gonine.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    //标题
+    private TextView tv_main_title;
+    //返回键,显示的注册，找回密码
+    private TextView tv_back;
+    //登录按钮
+    private Button btn_login;
+    //获取的用户名，密码，加密密码
+    private String userName,psw;
+    //用户名和密码的输入框
+    private EditText et_user_name,et_psw;
+
+    // for firebase auth
+    FirebaseAuth auth;
+
+    // for logging
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //设置此界面为？屏
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        auth = FirebaseAuth.getInstance();
+
+        init();
+    }
+
+
+
+    //获取界面控件
+    private void init() {
+        //从main_title_bar中获取的id
+        //从activity_login.xml中获取的
+        tv_back=findViewById(R.id.tv_back);
+        btn_login=findViewById(R.id.btn_login);
+        et_user_name=findViewById(R.id.et_user_name);
+        et_psw=findViewById(R.id.et_psw);
+        //返回键的点击事件
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //登录界面销毁
+                LoginActivity.this.finish();
+            }
+        });
+
+        //登录按钮的点击事件
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //开始登录，获取用户名和密码 getText().toString().trim();
+                userName=et_user_name.getText().toString().trim();
+                psw=et_psw.getText().toString().trim();
+
+                if(TextUtils.isEmpty(userName)){
+                    Toast.makeText(LoginActivity.this, "请输入用户名", Toast.LENGTH_SHORT).show();
+                    return;
+                }else if(TextUtils.isEmpty(psw)){
+                    Toast.makeText(LoginActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
+                    return;
+                    // md5Psw.equals(); 判断，输入的密码加密后，是否与保存在SharedPreferences中一致
+                }
+
+                auth.signInWithEmailAndPassword(userName, psw)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    Toast.makeText(LoginActivity.this, "Authentication succeed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+
+                            }
+                        });
+            }
+        });
+    }
+
+    /**
+     * 注册成功的数据返回至此
+     * @param requestCode 请求码
+     * @param resultCode 结果码
+     * @param data 数据
+     */
+    @Override
+    //显示数据， onActivityResult
+    //startActivityForResult(intent, 1); 从注册界面中获取数据
+    //int requestCode , int resultCode , Intent data
+    // LoginActivity -> startActivityForResult -> onActivityResult();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data!=null){
+            //是获取注册界面回传过来的用户名
+            // getExtra().getString("***");
+            String userName=data.getStringExtra("userName");
+            if(!TextUtils.isEmpty(userName)){
+                //设置用户名到 et_user_name 控件
+                et_user_name.setText(userName);
+                //et_user_name控件的setSelection()方法来设置光标位置
+                et_user_name.setSelection(userName.length());
+            }
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        // TODO: show corresponding UI for user
+        if(user != null){
+            Intent data=new Intent();
+            //datad.putExtra( ); name , value ;
+            data.putExtra("isLogin",true);
+            data.putExtra("userName",userName);
+            //RESULT_OK为Activity系统常量，状态码为-1
+            // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
+            setResult(RESULT_OK,data);
+            //销毁登录界面
+            LoginActivity.this.finish();
+            //跳转到主界面，登录成功的状态传递到 MainActivity 中
+            // TODO: update here
+            //startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
+        else{
+            // return to main activity
+            LoginActivity.this.finish();
+        }
+    }
+
+
+}
