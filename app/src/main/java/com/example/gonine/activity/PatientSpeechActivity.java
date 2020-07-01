150c007a0a2322243321300df74c76fc13433ac5
qky18 <qky18@mails.tsinghua.edu.cn>
2020-07-01 11:34:27 +0800

    add patientView, patientSpeech.

---
+++ app/src/main/java/com/example/gonine/activity/PatientSpeechActivity.java
@@ -0,0 +1,64 @@
+package com.example.gonine.activity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gonine.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class PatientSpeechActivity extends AppCompatActivity {
+    // TODO: add lineChart View
+    private ImageView pic;
+    private TextView name, gender, age;
+    private TextView speech_in, speech_formatted;
+    private Button btn_speak;
+
+    // for firebase auth
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_recognition);
+        //设置此界面为横屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        auth = FirebaseAuth.getInstance();
+
+        init();
+    }
+
+    //获取界面控件
+    private void init() {
+        Log.e("PatientSpeech", "init");
+        pic = findViewById(R.id.patient_pic);
+        name = findViewById(R.id.patient_name);
+        gender = findViewById(R.id.patient_gender);
+        age = findViewById(R.id.patient_age);
+        speech_in = findViewById(R.id.speech_input);
+        speech_formatted = findViewById(R.id.speech_output);
+        btn_speak = findViewById(R.id.btn_speak);
+        btn_speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: reaction to click
+
+            }
+        });
+
+        //TODO: set patient info
+        //e.g. pic.setBackground(xxx);
+        name.setText("002 Jennifer Kal");
+
+        //TODO: add Google Cloud speech recognition
+
+    }
+}
---
+++ app/src/main/java/com/example/gonine/activity/PatientViewActivity.java
@@ -0,0 +1,72 @@
+package com.example.gonine.activity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gonine.MainActivity;
+import com.example.gonine.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class PatientViewActivity extends AppCompatActivity {
+    // TODO: add lineChart View
+    private ImageButton btn_add_info;
+    private ImageView pic;
+    private TextView name, gender, age;
+    private TextView temperature, pressure, heart_rate, oxygen, breathe_rate;
+    private TextView patient_situ, symptom, doctor_advice;
+
+    // for firebase auth
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient);
+        //设置此界面为横屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        auth = FirebaseAuth.getInstance();
+
+        init();
+    }
+
+    //获取界面控件
+    private void init() {
+        Log.e("PatientView", "init");
+        pic = findViewById(R.id.patient_pic);
+        name = findViewById(R.id.patient_name);
+        gender = findViewById(R.id.patient_gender);
+        age = findViewById(R.id.patient_age);
+        temperature = findViewById(R.id.temperature);
+        pressure = findViewById(R.id.blood_pressure);
+        heart_rate = findViewById(R.id.heart_rate);
+        oxygen = findViewById(R.id.blood_oxygen);
+        breathe_rate = findViewById(R.id.breathe_rate);
+        patient_situ = findViewById(R.id.patient_situation);
+        symptom = findViewById(R.id.symptom);
+        doctor_advice = findViewById(R.id.doctor_advice);
+        btn_add_info = findViewById(R.id.btn_add_info);
+        btn_add_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(PatientViewActivity.this, PatientSpeechActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        //TODO: set patient info
+        //pic.setBackground(xxx);
+        patient_situ.setText("入院时间：2020年1月21日\n过往病史：糖尿病\n过敏史：N/A");
+
+        //TODO: add line chart binding & setting
+
+    }
+}
